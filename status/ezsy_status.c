133642a235ba14ee1875be4e09d37cb7eff1f0ee
Lipraxde <B10430214@mail.ntust.edu.tw>
2017-09-29 22:26:50 +0800

    Ezsy state machine.

---
+++ status/ezsy_status.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "status.h"
+
+void A(status_t *s);
+void B(status_t *s);
+
+void A(status_t *s)
+{
+    s->now_status = B;
+    printf("A");
+}
+
+void B(status_t *s)
+{
+    s->now_status = A;
+    printf("B");
+}
+
+int main(int argc, char *argv[])
+{
+    status_t s = {NULL, NULL, true, 0};
+
+    STATE_MACHINE(s, A);
+
+    return s.return_val;
+}
+
